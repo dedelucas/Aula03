b14d66fb91cf83b428299683c6fb0bbc37cff8ce
Andre Bruno <dede_lucas@yahoo.com.br>
2021-06-12 11:46:20 -0300

    Implementado o Operador Switch

--- src/EstruturaSwitch.java
+++ src/EstruturaSwitch.java
@@ -5,14 +5,33 @@ public class EstruturaSwitch {
 	public static void main(String[] args) {
 		/*Crie um programa que permita ao usuário escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio físico, 3 - Elogio pessoal
 		 * Caso digite qualquer outro número, deve ser exibida a mensagem "opção inválida"*/
+		
 		Scanner leitor = new Scanner(System.in);
+		
 		int opcao;
+		
 		System.out.println("ELOGIADOR!");
 		System.out.println("Selecione a opção desejada:\n1-Elogio profissional\n2-Elogio físico\n3-Elogio pessoal");
+		
 		opcao = leitor.nextInt();
 	
 		//Aqui precisamos de uma estrutura para validar a opção inserida.
 
+		switch(opcao) {
+		case 1: 
+			System.out.println("Os seus Ifs me emocionam");
+			break;
+		case 2:
+			System.out.println("A densidade dos seus ossos é invejável");
+			break;
+		case 3:
+			System.out.println("Voce é o melhor você que eu conheço!");
+		default:
+			System.out.println("Operação inválida");
+			break;
+		
+		}
+		
 		leitor.close();
 
 	}
